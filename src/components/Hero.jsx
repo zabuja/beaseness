acdf8fb267afa769bcb3a920359eb35e1fdd9825
zabuja <atanu.tmsl@gmail.com>
2022-10-26 21:23:51 +0530

    business section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
           className="w-[100%] h-[100%] relative z-[5]"
         />
 
-        {/* gradient start */}
+        {/* back gradient start ,z index lower than robo hand*/}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
